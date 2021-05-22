0c972f2bb69d5d61633f46fd34ac34056892e714
Sean Yuan <ysean54@yahoo.com>
2021-05-22 16:45:15 -0400

    MoreCodeWars

---
+++ strings.js
@@ -0,0 +1,33 @@
+//Return Strings - Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//function greet(name){
+//    return `Hello, ${name} how are you doing today?`
+//  }
+
+//reversed words - Complete the solution so that it reverses all of the words within the string passed in.
+
+//function reverseWords(str){
+//    return str.split(' ').reverse().join(' '); // reverse those words
+    
+//  }
+
+//Sum as Strings - Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+//function sumStr(a,b) {
+//    return Number(a)+Number(b)+''
+//  }
+
+//Abbreviate a Two Word Name - Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+//function abbrevName(name){
+
+//    [first,last] = name.toUpperCase().split(' ');
+//    return first[0] + '.' + last[0];
+  
+//  }
+
+//Multiplication table for number - Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+//function multiTable(number) {
+//    return [1,2,3,4,5,6,7,8,9,10].map(item => `${item} * ${number} = ${item*number}`).join('\n');
+//  }
